4e1635d9e5dddd98ede67c6baeded255b2c07891
masa-kazu <tashikazu1990@gmail.com>
2022-07-23 19:54:24 +0900

    new操作修正

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates :title, length: { in: 1..255 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_21_132320) do
+ActiveRecord::Schema.define(version: 2022_07_23_104716) do
 
   create_table "foods", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
-    t.boolean "complete", default: false, null: false
-    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,5 +32,4 @@ ActiveRecord::Schema.define(version: 2022_07_21_132320) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "foods", "users"
 end
