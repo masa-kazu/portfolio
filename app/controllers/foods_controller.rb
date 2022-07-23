c7a5cf76c6fcd0190dca5290357e1e22e9e1eb70
masa-kazu <tashikazu1990@gmail.com>
2022-07-23 16:58:30 +0900

    new画面作成

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,24 @@ class FoodsController < ApplicationController
   def index
     @foods = Food.all
   end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to foods_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def food_params
+    params.require(:food).permit(:title, :content, :complete)
+  end
+
+
 end
