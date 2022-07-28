2114613481f9147768da2b4c7fa3f2fedcda76b0
masa-kazu <tashikazu1990@gmail.com>
2022-07-28 19:39:32 +0900

    消費日機能追加

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,8 +1,8 @@
 class FoodsController < ApplicationController
   def index
     @today = Date.today
-    @foods_done = Food.where.not(done_at:nil) 
-    @foods = Food.where(done_at:nil).order(:time) 
+    @foods_done = Food.where.not(done_at:nil)
+    @foods = Food.where(done_at:nil).order(:time)
   end
 
   def new
@@ -24,7 +24,7 @@ class FoodsController < ApplicationController
 
   def update
     @food = Food.find(params[:id])
-    if @food.update(food_params) 
+    if @food.update(food_params)
       redirect_to foods_path#updateできたらindexページに行く
     else
       render 'edit'#できなかったらeditページのまま
@@ -40,7 +40,7 @@ class FoodsController < ApplicationController
   def done
     @today = Date.today #今日の日付
     food = Food.find(params[:id])
-    food.update(done_at: @food ) #完了ボタンを押すとdone_atカラムに完了した日時を追加
+    food.update(done_at: @today )#完了ボタンを押すとdone_atカラムに完了した日時を追加
     redirect_to :action => "index"
   end
 
