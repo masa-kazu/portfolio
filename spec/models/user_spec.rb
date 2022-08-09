d2df442dc84436a017363c64c85731be065b9e7d
masa-kazu <tashikazu1990@gmail.com>
2022-08-09 21:59:19 +0900

    モデルテスト追加

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "名、メール、パスワードがある場合、有効である" do
+    user = User.new(
+      name: "tarou",
+      email: "testman@example.com",
+      password: "password",
+      password_confirmation: "password"
+      )
+    expect(user).to be_valid
+  end
+  it "名がない場合、無効である" do
+  end
+end
