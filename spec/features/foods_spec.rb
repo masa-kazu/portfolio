0630eaec3ec2760b36a6a685a4160921ae224714
masa-kazu <tashikazu1990@gmail.com>
2022-08-16 21:17:15 +0900

    featuresテスト修正

--- spec/features/foods_spec.rb
+++ spec/features/foods_spec.rb
@@ -3,20 +3,16 @@ require 'rails_helper'
 RSpec.feature "Foods", type: :feature do
   describe "food details" do
     before do
-      user = User.new(
-        name: "tarou",
-        email: "testman@example.com",
-        password: "password",
-        password_confirmation: "password"
-        )
-      sign_in user
+      @user = FactoryBot.create(:user)
     end
     it "食品一覧表示確認" do
+      sign_in @user
       visit foods_path
       expect(page).to have_content "食品一覧"
     end
 
     it "食品新規作成確認" do
+      sign_in @user
       visit new_food_path
       expect(page).to have_content "食品新規作成"
       fill_in "food[title]",         with: "Test"
