746cf965d5424ca0e55564ba8556c0cb22e2737b
masa-kazu <tashikazu1990@gmail.com>
2022-08-16 21:43:46 +0900

    featuresテスト修正

--- spec/features/foods_spec.rb
+++ spec/features/foods_spec.rb
@@ -4,20 +4,12 @@ RSpec.feature "Foods", type: :feature do
   describe "food details" do
     before do
       @user = FactoryBot.create(:user)
-      @food = FactoryBot.build(:food)
     end
 
     it "食品一覧表示確認" do
       sign_in @user
       visit foods_path
       expect(page).to have_content "食品一覧"
-    end
-
-    it "食品一覧編集確認" do
-      sign_in @user
-      visit foods_path
-      click_button '編集'
-      expect(current_path).to eq edit_food_path(food)
     end
 
     it "食品新規作成確認" do
@@ -29,6 +21,7 @@ RSpec.feature "Foods", type: :feature do
       fill_in "food[time]",     with: "002022-10-06"
       click_button '追加'
       expect(current_path).to eq foods_path
+      expect(page).to have_content "編集"
     end
 
   end
