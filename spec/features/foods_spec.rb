a08a6a2a9348d4ea72b4367209ce52dc49f1f897
masa-kazu <tashikazu1990@gmail.com>
2022-08-16 21:12:45 +0900

    featuresテスト追加

--- spec/features/foods_spec.rb
+++ spec/features/foods_spec.rb
@@ -10,10 +10,20 @@ RSpec.feature "Foods", type: :feature do
         password_confirmation: "password"
         )
       sign_in user
-      visit foods_path
     end
     it "食品一覧表示確認" do
+      visit foods_path
       expect(page).to have_content "食品一覧"
+    end
+
+    it "食品新規作成確認" do
+      visit new_food_path
+      expect(page).to have_content "食品新規作成"
+      fill_in "food[title]",         with: "Test"
+      fill_in "food[content]",        with: "test"
+      fill_in "food[time]",     with: "002022-10-06"
+      click_button '追加'
+      expect(current_path).to eq foods_path
     end
 
   end
