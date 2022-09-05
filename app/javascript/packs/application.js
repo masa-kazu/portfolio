f836e9492f004ca4acbbb1c30eb2c14e89f8cfcd
masa-kazu <tashikazu1990@gmail.com>
2022-09-05 21:56:43 +0900

    レイアウト修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,6 @@ import "channels";
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+
+const images = require.context("../images", true);
+const imagePath = (name) => images(name, true);
